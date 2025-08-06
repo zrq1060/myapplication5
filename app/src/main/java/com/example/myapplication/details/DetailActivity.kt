fe9350e644d6383aa7d1dc81057c435b09f3c6b3
zhangrq <zrq1060@163.com>
2025-08-06 11:05:50 +0800

    详情改为ViewPager2实现

--- app/src/main/java/com/example/myapplication/details/DetailActivity.kt
+++ app/src/main/java/com/example/myapplication/details/DetailActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import android.view.ViewTreeObserver
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.bean.ItemData
+import androidx.viewpager2.widget.ViewPager2
 import com.example.myapplication.R
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.example.myapplication.bean.ItemData
 
 class DetailActivity : AppCompatActivity() {
 
@@ -22,23 +18,23 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         // 获取传递的数据
-        var targetPosition = intent.getIntExtra("position", 0)
+        val targetPosition = intent.getIntExtra("position", 0)
         val itemList = intent.getSerializableExtra("allData") as List<ItemData>
 
         // 配置水平RecyclerView
-        val recyclerView = findViewById<RecyclerView>(R.id.detail_recycler)
+        val viewPager2 = findViewById<ViewPager2>(R.id.detail_viewpager2)
         val adapter = DetailAdapter(itemList)
-        recyclerView.setAdapter(adapter)
+        viewPager2.setAdapter(adapter)
 
         // 滚动到点击的位置
-        recyclerView.scrollToPosition(targetPosition)
+        viewPager2.setCurrentItem(targetPosition, false)
 
-        // 等待RecyclerView布局完成后再开始过渡动画
+        // 等待ViewPager2布局完成后再开始过渡动画
         postponeEnterTransition()
-        recyclerView.getViewTreeObserver()
+        viewPager2.viewTreeObserver
             .addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
                 override fun onPreDraw(): Boolean {
-                    recyclerView.getViewTreeObserver().removeOnPreDrawListener(this)
+                    viewPager2.viewTreeObserver.removeOnPreDrawListener(this)
                     // 开始过渡动画
                     // 可以延迟几秒后再设置，那就在这个页面等待几秒中，然后再执行动画。
                     startPostponedEnterTransition()
